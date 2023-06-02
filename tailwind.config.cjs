1407fc40cd8dfc6eba85349bf6a07d8f75998010
dethMastery <me@suphakit.net>
2023-06-02 20:41:15 +0700

    chore: config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,7 @@ module.exports = {
       whitesmoke: "#f5f5f5",
       "living-coral": "#fe6e61",
       overlay: "rgba(0,0,0,0.4)",
+      skyBlue: '#7ec4cf'
     },
     borderRadius: {
       DEFAULT: "1rem",
